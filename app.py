b06e2fab2a48a07a69925df59ac63fceab2602a8
krishnanshagarwal112 <gonkillua2680@gmail.com>
2023-03-20 15:36:57 +0530

    finished

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def handle_message(message):
     socketio.emit('messageFromServer', message,broadcast=True, include_self=False)
 
 if __name__ == '__main__':
-    socketio.run(app,debug=False,host='0.0.0.0')
+    socketio.run(app,debug=False)
